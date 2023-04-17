0f9e42387d621cbd9fcc1e4799641f85ba296ba8
Richarth Atausinchi F <sinchijackal@gmail.com>
2023-04-17 15:33:06 -0500

    updated

---
+++ convert_ts_mp4.py
@@ -0,0 +1,30 @@
+import subprocess
+import os
+import glob
+
+ruta_ffmpeg = 'D:\\ffmpeg\\bin\\ffmpeg.exe'
+carpeta_videos_audios = r'D:\Cursos\code-easy'
+carpeta_salida_fusion = r'D:\Cursos\code-easy\salida'
+
+# Obtener los archivos de la carpeta
+archivos = os.listdir(carpeta_videos_audios)
+prefijos = []  # Lista de prefijos de los archivos de video ejm: [2, 3, "9b", 10, 14, 15, 16, "16b", 21, 22]
+
+archivos = sorted(archivos)
+# Leemos los archivos .mp4 y obtenemos los prefijos
+for archivo in archivos:
+    if archivo.endswith('.ts'):
+        prefijo = archivo.split('-')[0]
+        if prefijo not in prefijos:
+            prefijos.append(prefijo)
+
+# Procedemos a fusionar a goku y vegeta
+for i in prefijos:
+    archivo_video = glob.glob(os.path.join(carpeta_videos_audios, f"{i}-*.ts"))[0]
+    nombre_archivo_video = os.path.splitext(os.path.basename(archivo_video))[0]
+    # cadena_sin_subcadena = cadena.replace("(720p with 30fps)", "")
+    nombre_archivo_video = nombre_archivo_video.replace(" - Curso Profesional de Typescript-playlist", "")
+    archivo_salida = os.path.join(carpeta_salida_fusion, f"{nombre_archivo_video}.mp4")
+    # invocamos a ffmpeg para la fusion
+    print("\033[32m" + f"Procesando: {archivo_video} -> {archivo_salida}" + "\033[0m")
+    subprocess.run(f"{ruta_ffmpeg} -i \"{archivo_video}\" -c:v libx264 \"{archivo_salida}\"")
