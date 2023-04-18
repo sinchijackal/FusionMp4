32333f42e3d7ace21e8fa1680ebafbfd565beb2d
Richarth Atausinchi F <sinchijackal@gmail.com>
2023-04-17 23:09:43 -0500

    updated

--- convert_ts_mp4.py
+++ convert_ts_mp4.py
@@ -3,11 +3,15 @@ import os
 import glob
 
 ruta_ffmpeg = 'D:\\ffmpeg\\bin\\ffmpeg.exe'
-carpeta_videos_audios = r'D:\Cursos\code-easy'
-carpeta_salida_fusion = r'D:\Cursos\code-easy\salida'
+carpeta_videos_ts = r'D:\Cursos\code-easy'
+carpeta_salida_conversion = r'D:\Cursos\code-easy\salida'
+
+# Verificar si existe la carpeta de salida y si no existe la creamos
+if not os.path.exists(carpeta_salida_conversion):
+    os.makedirs(carpeta_salida_conversion)
 
 # Obtener los archivos de la carpeta
-archivos = os.listdir(carpeta_videos_audios)
+archivos = os.listdir(carpeta_videos_ts)
 prefijos = []  # Lista de prefijos de los archivos de video ejm: [2, 3, "9b", 10, 14, 15, 16, "16b", 21, 22]
 
 archivos = sorted(archivos)
@@ -20,11 +24,11 @@ for archivo in archivos:
 
 # Procedemos a convertir el archivo .ts a .mp4
 for i in prefijos:
-    archivo_video = glob.glob(os.path.join(carpeta_videos_audios, f"{i}-*.ts"))[0]
+    archivo_video = glob.glob(os.path.join(carpeta_videos_ts, f"{i}-*.ts"))[0]
     nombre_archivo_video = os.path.splitext(os.path.basename(archivo_video))[0]
     # cadena_sin_subcadena = cadena.replace("(720p with 30fps)", "")
     nombre_archivo_video = nombre_archivo_video.replace(" - Curso de React con Typescript-playlist", "")
-    archivo_salida = os.path.join(carpeta_salida_fusion, f"{nombre_archivo_video}.mp4")
+    archivo_salida = os.path.join(carpeta_salida_conversion, f"{nombre_archivo_video}.mp4")
     # invocamos a ffmpeg para la conversión
     print("\033[37m" + f"Convirtiendo... \033[32m{archivo_video}\033[37m --> \033[33m{archivo_salida}" + "\033[0m")
 
