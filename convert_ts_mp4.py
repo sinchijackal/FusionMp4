7abcf7ca0dd1ae311769aab88403b5ab96ad7746
Richarth Atausinchi F <sinchijackal@gmail.com>
2023-04-17 22:55:07 -0500

    updated

--- convert_ts_mp4.py
+++ convert_ts_mp4.py
@@ -18,13 +18,13 @@ for archivo in archivos:
         if prefijo not in prefijos:
             prefijos.append(prefijo)
 
-# Procedemos a fusionar a goku y vegeta
+# Procedemos a convertir el archivo .ts a .mp4
 for i in prefijos:
     archivo_video = glob.glob(os.path.join(carpeta_videos_audios, f"{i}-*.ts"))[0]
     nombre_archivo_video = os.path.splitext(os.path.basename(archivo_video))[0]
     # cadena_sin_subcadena = cadena.replace("(720p with 30fps)", "")
     nombre_archivo_video = nombre_archivo_video.replace(" - Curso de React con Typescript-playlist", "")
     archivo_salida = os.path.join(carpeta_salida_fusion, f"{nombre_archivo_video}.mp4")
-    # invocamos a ffmpeg para la fusion
+    # invocamos a ffmpeg para la conversión
     print("\033[32m" + f"Convirtiendo... {archivo_video} --> {archivo_salida}" + "\033[0m")
     subprocess.run(f"{ruta_ffmpeg} -hide_banner -nostats -loglevel error -i \"{archivo_video}\" -c:v libx264 \"{archivo_salida}\"")
